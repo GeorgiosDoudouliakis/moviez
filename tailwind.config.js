43ffc565a67e3e3f7c651803a22674eec4f3394e
GeorgiosDoudouliakis <dudugeorge89@gmail.com>
2025-01-11 16:43:48 +0200

    Change colors of tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors: {
+      'primary': '#f3c71e',
+      'secondary': '#f9f9f9',
+      'bg-color': '#000000',
+      'card-bg': '#1a1a1a'
+    },
     extend: {},
   },
   plugins: [],
