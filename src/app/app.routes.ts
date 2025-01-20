36536473d9e3343532ae0b0492af24a10e590ddf
GeorgiosDoudouliakis <dudugeorge89@gmail.com>
2025-01-20 21:39:26 +0200

    Add components and routing for movies and tv series and add active classes for links on header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,13 @@ export const routes: Routes = [
     {
         path: "",
         loadComponent: () => import("./pages/home/home.component").then(c => c.HomeComponent),
+    },
+    {
+        path: "movies",
+        loadComponent: () => import("./pages/movies/movies/movies.component").then(c => c.MoviesComponent)
+    },
+    {
+        path: "tv-series",
+        loadComponent: () => import("./pages/tv-series/tv-series/tv-series.component").then(c => c.TvSeriesComponent)
     }
 ];
