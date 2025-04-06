91dbb9635ca406526d85d532bcfd9be1d757bc28
GeorgiosDoudouliakis <dudugeorge89@gmail.com>
2025-04-06 14:31:27 +0300

    Add a background color for header on tailwind config, use it on header, remove border from header and mobile menu, use italic font on h1 elements and uppercase header titles on movies and tv series pages

--- src/app/pages/home/shared/components/section-header/section-header.component.ts
+++ src/app/pages/home/shared/components/section-header/section-header.component.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-section-header',
-  imports: [RouterLink],
+  imports: [RouterLink, UpperCasePipe],
   templateUrl: './section-header.component.html',
   styleUrl: './section-header.component.scss'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
       'secondary': '#f8ffff',
       'darker-secondary': '#dadada',
       'blue': '#6697E5',
-      'bg-color': '#121212'
+      'bg-color': '#121212',
+      'header-bg-color': '#1F1F1F'
     },
     extend: {},
   },
