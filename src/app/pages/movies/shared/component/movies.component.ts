da072d3978631e525e87ccd8694b5f0ea487b357
GeorgiosDoudouliakis <dudugeorge89@gmail.com>
2025-04-26 22:06:24 +0300

    Remove italic font from h2 elements, implement show and hide filters with button on movies component and add on hover styles on load more button

--- src/app/pages/movies/shared/component/movies.component.ts
+++ src/app/pages/movies/shared/component/movies.component.ts
@@ -4,22 +4,27 @@ import { Movie } from '@shared/interfaces/movies-response.interface';
 import { CardComponent } from '@shared/components/card/card.component';
 import { SkeletonComponent } from '@shared/components/skeleton/skeleton.component';
 import { LoadingState } from '@shared/enums/loading-state.enum';
-import {NgClass} from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-movies',
-  imports: [GenresComponent, CardComponent, SkeletonComponent, NgClass],
+  imports: [GenresComponent, CardComponent, SkeletonComponent, NgClass, NgStyle],
   templateUrl: 'movies.component.html',
   styleUrl: '../../../../shared/directives/movies-tv-series/movies-tv-series.directive.scss'
 })
 export class MoviesComponent {
-  public viewType: WritableSignal<"grid" | "list"> = signal<"grid" | "list">("grid");
-  public title: InputSignal<string> = input.required<string>();
-  public items: InputSignal<Movie[]> = input.required<Movie[]>();
-  public showLoadMore: InputSignal<boolean> = input.required<boolean>();
-  public loadingState: InputSignal<LoadingState | null> = input.required<LoadingState | null>();
+  public areFiltersVisible: WritableSignal<boolean> = signal(false);
+  public viewType: WritableSignal<"grid" | "list"> = signal("grid");
+  public title: InputSignal<string> = input.required();
+  public items: InputSignal<Movie[]> = input.required();
+  public showLoadMore: InputSignal<boolean> = input.required();
+  public loadingState: InputSignal<LoadingState | null> = input.required();
   public loadMore: OutputEmitterRef<void> = output();
   public readonly LoadingState: typeof LoadingState = LoadingState;
+
+  public onFiltersVisibilityChange(): void {
+    this.areFiltersVisible.set(!this.areFiltersVisible());
+  }
 
   public onLoadMore(): void {
     this.loadMore.emit();
