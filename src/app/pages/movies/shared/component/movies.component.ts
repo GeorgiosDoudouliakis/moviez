de753d10cd051b332bb27bd8efda260d2bcbfbe2
GeorgiosDoudouliakis <dudugeorge89@gmail.com>
2025-04-26 21:29:22 +0300

    View type change implementation on movies component and style changes on card component

--- src/app/pages/movies/shared/component/movies.component.ts
+++ src/app/pages/movies/shared/component/movies.component.ts
@@ -1,17 +1,19 @@
-import { Component, input, InputSignal, output, OutputEmitterRef } from '@angular/core';
+import { Component, input, InputSignal, output, OutputEmitterRef, signal, WritableSignal } from '@angular/core';
 import { GenresComponent } from '@shared/components/genres/genres.component';
 import { Movie } from '@shared/interfaces/movies-response.interface';
 import { CardComponent } from '@shared/components/card/card.component';
 import { SkeletonComponent } from '@shared/components/skeleton/skeleton.component';
 import { LoadingState } from '@shared/enums/loading-state.enum';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-movies',
-  imports: [GenresComponent, CardComponent, SkeletonComponent],
+  imports: [GenresComponent, CardComponent, SkeletonComponent, NgClass],
   templateUrl: 'movies.component.html',
   styleUrl: '../../../../shared/directives/movies-tv-series/movies-tv-series.directive.scss'
 })
 export class MoviesComponent {
+  public viewType: WritableSignal<"grid" | "list"> = signal<"grid" | "list">("grid");
   public title: InputSignal<string> = input.required<string>();
   public items: InputSignal<Movie[]> = input.required<Movie[]>();
   public showLoadMore: InputSignal<boolean> = input.required<boolean>();
@@ -21,5 +23,9 @@ export class MoviesComponent {
 
   public onLoadMore(): void {
     this.loadMore.emit();
+  }
+
+  public onViewTypeChange(type: "grid" | "list"): void {
+    this.viewType.set(type);
   }
 }
