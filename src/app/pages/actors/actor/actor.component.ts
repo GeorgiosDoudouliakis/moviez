c4e73102217408cb6968303778c1e518fdfcf0e7
GeorgiosDoudouliakis <dudugeorge89@gmail.com>
2025-05-07 22:22:27 +0300

    wip: Actor page

--- src/app/pages/actors/actor/actor.component.ts
+++ src/app/pages/actors/actor/actor.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnInit, signal, WritableSignal } from '@angular/core';
+import {ActivatedRoute, RouterLink} from '@angular/router';
+import { catchError, forkJoin, tap, throwError } from 'rxjs';
+import { ActorService } from './service/actor.service';
+import { Person } from '@shared/interfaces/persons-response.interface';
+import { PersonDetails } from './interfaces/people-response.interface';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { LoaderComponent } from '@shared/components/loader/loader.component';
 
 @Component({
   selector: 'app-actor',
-  imports: [],
+  imports: [
+    LoaderComponent,
+    RouterLink
+  ],
   templateUrl: './actor.component.html',
-  styleUrl: './actor.component.scss'
+  styleUrl: './actor.component.scss',
+  providers: [ActorService]
 })
-export class ActorComponent {
+export class ActorComponent implements OnInit {
+  public actor: WritableSignal<Person | null> = signal(null);
+  public actorDetails: WritableSignal<PersonDetails | null> = signal(null);
+  public loading: WritableSignal<boolean> = signal(true);
 
+  constructor(
+    private readonly _service: ActorService,
+    private readonly _route: ActivatedRoute,
+    private readonly _destroyRef: DestroyRef
+  ) {}
+
+  public ngOnInit(): void {
+    const paramsArr: string[] = this._route.snapshot.params["actor"].split("-");
+
+    forkJoin({
+      actor: this._service.actor$(paramsArr.slice(1).join(" ")),
+      actorDetails: this._service.actorDetails$(+paramsArr[0])
+    }).pipe(
+      tap(({ actor, actorDetails }) => {
+        this.actor.set(actor);
+        this.actorDetails.set(actorDetails);
+      }),
+      tap(() => this.loading.set(false)),
+      catchError(error => {
+        this.loading.set(false);
+        return throwError(() => error);
+      }),
+      takeUntilDestroyed(this._destroyRef)
+    ).subscribe();
+  }
 }
